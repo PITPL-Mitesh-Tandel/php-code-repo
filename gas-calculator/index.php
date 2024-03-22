a5687a695f0b57ef6ea4094a94f1a3267840e784
PITPL-Mitesh-Tandel <mitesh@pseudocode.co>
2024-03-22 17:14:09 +0530

    Calculator added.

---
+++ gas-calculator/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gas Calculator</title>
+  </head>
+  <body>
+    <div id="calculatorWidget"></div>
+    <script type="text/javascript" src="./gas-calculator.min.js"></script>
+    <script type="text/javascript">
+      GasCalculator.init('#calculatorWidget');
+    </script>
+  </body>
+</html>
