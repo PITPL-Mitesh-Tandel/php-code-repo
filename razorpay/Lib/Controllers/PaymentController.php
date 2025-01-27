246c05a65c101725082841b4e3d37860892c97c9
PITPL-Mitesh-Tandel <mitesh@pseudocode.co>
2025-01-27 19:10:36 +0530

    Cors added.

--- razorpay/Lib/Controllers/PaymentController.php
+++ razorpay/Lib/Controllers/PaymentController.php
@@ -16,6 +16,9 @@ class PaymentController extends Controller
     public function makePayment()
     {
         header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
 
         $data = json_decode(file_get_contents('php://input'), true);
 
