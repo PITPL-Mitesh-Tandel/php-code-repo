83ae871ee464468526bd8b063a9ff9d0753a859e
PITPL-Mitesh-Tandel <mitesh@pseudocode.co>
2025-02-25 10:28:07 +0530

    Removed unwanted projects.

--- atelier-front/mailtest.php
+++
@@ -1,4 +0,0 @@
-<?php
-//mail("hiral1982@gmail.com",'Hello Paris','Hello Paris') or die('it is testing');
-mail("abdou.kriaa@gmail.com",'Hello Paris','Hello Paris for ATELIER DADA') or die('it is testing');
-?>
